08414ea97890d6dfd8b3ac23d3035b398b47bd5c
Maidana <franco_maidana07@hotmail.com>
2024-08-22 21:41:34 -0300

    Cambiando estilos y layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -7,6 +7,8 @@ const Layout = ({ children }) => {
             <Head>
                 <title>Alisados L.M</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Renueva tu cabello con Alisados L.M. Ofrecemos tratamientos capilares de alta calidad que nutren, reparan y alisan, dejándote un cabello radiante. Experimenta un servicio personalizado con pasión y dedicación. ¡Siéntete cómoda, segura y luce un cabello espectacular!" />
+                <meta name="author" content="Franco y Miguel" />
             </Head>
             <Navbar />
             <main>
